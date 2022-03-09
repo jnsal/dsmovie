22b7e660b6f0fa9e31653d5a93b70de51153a1bb
AloneTheKing <jonasgomes_123@hotmail.com>
2022-03-09 18:16:31 -0400

    Initial Commit

---
+++ dsmovie/backend/src/main/java/com/devsuperior/dsmovie/JonasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JonasApplication.class, args);
+	}
+
+}
