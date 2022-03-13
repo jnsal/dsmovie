69caf801c7cd3f58b0c1246317aea9a04b343b5a
AloneTheKing <jonasgomes_123@hotmail.com>
2022-03-13 10:11:01 -0400

    useParams

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,21 +1,29 @@
-import {ReactComponent as GitHubIcon} from 'assets/img/github.svg';
+import { ReactComponent as GitHubIcon } from 'assets/img/github.svg';
+// import { useParams } from 'react-router-dom';
+import { HOME_URL } from 'utils/requests';
 import './styles.css'
+
+
 function Navbar() {
-    return (
-        <header>
-          <nav className="container">
-            <div className="dsmovie-nav-content">
-              <h1>DSMovie</h1>
-              <a href="https://github.com/jnsal">
-                <div className="dsmovie-contact-container">
-                  <GitHubIcon />
-                  <p className="dsmovie-contact-link">/jnsal</p>
-                </div>
-                </a>
+  // const params = new URLSearchParams(window.location.search)
+
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <a href={`${HOME_URL}`}>
+            <h1>DSMovie</h1>
+          </a>
+          <a href="https://github.com/jnsal">
+            <div className="dsmovie-contact-container">
+              <GitHubIcon />
+              <p className="dsmovie-contact-link">/jnsal</p>
             </div>
-          </nav>
-        </header>
-      );
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,2 @@
 export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080"
+export const HOME_URL = process.env.HOME_URL ?? "http://localhost:3000" // ??
